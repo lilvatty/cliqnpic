4cdf16c521afeba0d66931991f2030ba81f12889
lilvatty <kmlinggamahesa@gmail.com>
2025-04-14 16:41:30 +0800

    delete database, and backend stuff

--- prisma/migrations/20250322121306_/migration.sql
+++
@@ -1,8 +0,0 @@
-/*
-  Warnings:
-
-  - You are about to alter the column `image` on the `Products` table. The data in that column could be lost. The data in that column will be cast from `VarChar(191)` to `Json`.
-
-*/
--- AlterTable
-ALTER TABLE `Products` MODIFY `image` JSON NOT NULL;
